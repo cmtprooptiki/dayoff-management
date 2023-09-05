41002bf8db0a3a3e3cb6fc2a475f29ac3b60280e
A-PALIOS <paliosadonis6@gmail.com>
2023-09-05 15:21:16 +0300

    message

---
+++ app1.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import subprocess
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+)
+st.write("# Welcome to Streamlit! 👋")
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
+# Create an empty container
+placeholder = st.empty()
+
+actual_email = "email"
+actual_password = "password"
+
+# Insert a form in the container
+with placeholder.form("login"):
+    st.markdown("#### Enter your credentials")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submit = st.form_submit_button("Login")
+
+if submit and email == actual_email and password == actual_password:
+    # If the form is submitted and the email and password are correct,
+    # clear the form/container and display a success message
+    placeholder.empty()
+    st.success("Login successful")
+    # Run the authenticated app as a separate process
+    subprocess.Popen(["streamlit", "run", "app2.py"])
+elif submit and email != actual_email and password != actual_password:
+    st.error("Login failed")
+else:
+    pass
