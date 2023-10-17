5c105d48e2ba9a8e143aadae71a0d2986b6fffc1
A-PALIOS <paliosadonis6@gmail.com>
2023-10-17 11:37:23 +0300

    message

--- app.py
+++ app.py
@@ -31,8 +31,9 @@ def main():
         st.session_state.login_state = False
     
     if not st.session_state.login_state:
-        login(conn)
-        # st.write(var)
+        var=login(conn)
+        st.write(var)
+        st.experimental_rerun()
     else:
         mainpage(conn)
 
@@ -61,28 +62,28 @@ def main():
 #         st.error("Login failed")
 #     else:
 #         pass 
-def submited(conn,email):
-    actual_email = "email"
-    actual_password = "password"
+# def submited(conn,email):
+#     actual_email = "email"
+#     actual_password = "password"
 
-    # if email == actual_email and password == actual_password:
-    #     st.session_state.login_state = True
-    #     st.success("Login successful")
-    # else:
-    #     st.error("Login failed")
-    sql = f"""
-            SELECT kimai2_users.username, kimai2_users.password FROM `kimai2_users` WHERE kimai2_users.username = '{email}';    
-        """
-    rows,columnames = run_query(conn,sql)
-    # st.write(columnames)
-    dflogin=pd.DataFrame(rows,columns=columnames)
-    if len(dflogin)>0:
-        st.session_state.login_state = True
-        # st.success("Login successful")
-        # st.write(st.session_state.login_state)
+#     # if email == actual_email and password == actual_password:
+#     #     st.session_state.login_state = True
+#     #     st.success("Login successful")
+#     # else:
+#     #     st.error("Login failed")
+#     sql = f"""
+#             SELECT kimai2_users.username, kimai2_users.password FROM `kimai2_users` WHERE kimai2_users.username = '{email}';    
+#         """
+#     rows,columnames = run_query(conn,sql)
+#     # st.write(columnames)
+#     dflogin=pd.DataFrame(rows,columns=columnames)
+#     if len(dflogin)>0:
+#         st.session_state.login_state = True
+#         # st.success("Login successful")
+#         # st.write(st.session_state.login_state)
         
-    else:
-        st.error("Login failed")
+#     else:
+#         st.error("Login failed")
 
 def login(conn):
     
@@ -92,34 +93,34 @@ def login(conn):
         st.title("Day Off Management")
         email = st.text_input("Email")
         #password = st.text_input("Password", type="password")
-        submit = st.form_submit_button("Login",on_click=submited(conn,email))
+        submit = st.form_submit_button("Login")
         
         
 
         
-        # if submit:
-        #     actual_email = "email"
-        #     actual_password = "password"
+        if submit:
+            actual_email = "email"
+            actual_password = "password"
 
-        #     # if email == actual_email and password == actual_password:
-        #     #     st.session_state.login_state = True
-        #     #     st.success("Login successful")
-        #     # else:
-        #     #     st.error("Login failed")
-        #     sql = f"""
-        #          SELECT kimai2_users.username, kimai2_users.password FROM `kimai2_users` WHERE kimai2_users.username = '{email}';    
-        #         """
-        #     rows,columnames = run_query(conn,sql)
-        #     # st.write(columnames)
-        #     dflogin=pd.DataFrame(rows,columns=columnames)
-        #     if len(dflogin)>0:
-        #         st.session_state.login_state = True
-        #         # st.success("Login successful")
-        #         # st.write(st.session_state.login_state)
-        #         return 0
+            # if email == actual_email and password == actual_password:
+            #     st.session_state.login_state = True
+            #     st.success("Login successful")
+            # else:
+            #     st.error("Login failed")
+            sql = f"""
+                 SELECT kimai2_users.username, kimai2_users.password FROM `kimai2_users` WHERE kimai2_users.username = '{email}';    
+                """
+            rows,columnames = run_query(conn,sql)
+            # st.write(columnames)
+            dflogin=pd.DataFrame(rows,columns=columnames)
+            if len(dflogin)>0:
+                st.session_state.login_state = True
+                # st.success("Login successful")
+                # st.write(st.session_state.login_state)
+                return 0
                 
-        #     else:
-        #         st.error("Login failed")
+            else:
+                st.error("Login failed")
         
 
 def mainpage(conn):
