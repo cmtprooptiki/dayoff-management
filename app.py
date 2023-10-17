7dd0b9762b49e45b5bf3e86a9a1d2ff235cf6ce1
A-PALIOS <paliosadonis6@gmail.com>
2023-10-17 11:29:36 +0300

    message

--- app.py
+++ app.py
@@ -97,29 +97,29 @@ def login(conn):
         
 
         
-        # if submit:
-        #     actual_email = "email"
-        #     actual_password = "password"
+        if submit:
+            actual_email = "email"
+            actual_password = "password"
 
-        #     # if email == actual_email and password == actual_password:
-        #     #     st.session_state.login_state = True
-        #     #     st.success("Login successful")
-        #     # else:
-        #     #     st.error("Login failed")
-        #     sql = f"""
-        #          SELECT kimai2_users.username, kimai2_users.password FROM `kimai2_users` WHERE kimai2_users.username = '{email}';    
-        #         """
-        #     rows,columnames = run_query(conn,sql)
-        #     # st.write(columnames)
-        #     dflogin=pd.DataFrame(rows,columns=columnames)
-        #     if len(dflogin)>0:
-        #         st.session_state.login_state = True
-        #         # st.success("Login successful")
-        #         # st.write(st.session_state.login_state)
-        #         return 0
+            # if email == actual_email and password == actual_password:
+            #     st.session_state.login_state = True
+            #     st.success("Login successful")
+            # else:
+            #     st.error("Login failed")
+            sql = f"""
+                 SELECT kimai2_users.username, kimai2_users.password FROM `kimai2_users` WHERE kimai2_users.username = '{email}';    
+                """
+            rows,columnames = run_query(conn,sql)
+            # st.write(columnames)
+            dflogin=pd.DataFrame(rows,columns=columnames)
+            if len(dflogin)>0:
+                st.session_state.login_state = True
+                # st.success("Login successful")
+                # st.write(st.session_state.login_state)
+                return 0
                 
-        #     else:
-        #         st.error("Login failed")
+            else:
+                st.error("Login failed")
         
 
 def mainpage(conn):
